c25bb59a01211231051602929a4612f812bd4aa5
05 <im05@kakao.com>
2020-04-29 21:17:50 +0900

    slightly changed

--- Tree/DFS.js
+++ Tree/DFS.js
@@ -23,5 +23,14 @@ function DFS(tree) {
   traverse(tree.root);
   return data;
 }
-
+function DFSPostOrder(tree) {
+  let data = [];
+  function traverse(node) {
+    if (node.left) traverse(node.left);
+    if (node.right) traverse(node.right);
+    data.push(node.value);
+  }
+  traverse(tree.root);
+  return data;
+}
 console.log(DFS(tree));
